769c58c4d74a23ca1230e7b7a9ae60ba47a3153e
OLGA <30398249+Olga-Pa@users.noreply.github.com>
2021-05-23 17:31:00 -0400

    po

--- src/components/ToDoForm.tsx
+++ src/components/ToDoForm.tsx
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
-import React, { useRef } from 'react'
-=======
+
 import React, {useRef} from 'react';
->>>>>>> 7adc6b4192478d9e7b82afa786e1cf0f81813c5f
 
 interface TodoFormProps {
   onAdd(title: string): void
 }
 
 export const TodoForm: React.FC<TodoFormProps> = props => {
-<<<<<<< HEAD
   // const [title, setTitle] = useState<string>('')
   const ref = useRef<HTMLInputElement>(null)
 
@@ -44,37 +40,3 @@ export const TodoForm: React.FC<TodoFormProps> = props => {
   )
 }
 
-
-=======
- // const [title, setTitle] = useState<string>('')
-  const ref = useRef<HTMLInputElement>(null)
-  //const changeHandler = (event: React.ChangeEvent<HTMLInputElemen
-  //  setTitle(event.target.value)
-  //}
-
-  const keyPressHander = (event: React.KeyboardEvent) => {
-    if (event.key === "Enter") {
-      props.onAdd(ref.current!.value)
-      ref.current!.value = ''
-      //setTitle('')
-      //console.log({ changeHandler })
-    }
-  }
-
-    return (
-      <div className="Input-file">
-        <label htmlFor="title" className="active mf2">What do  you want to do?</label>
-        <input
-          //onChange={ref}
-         // value={title}
-          ref={ref}
-          type="text"
-          id="title"
-          placeholder="what do you want to do"
-          onKeyPress={keyPressHander}>
-        </input>
-
-      </div>
-    )
-  }
->>>>>>> 7adc6b4192478d9e7b82afa786e1cf0f81813c5f
