b8e5cc616a0e10dd8a7e654bc8fdfec175b91fac
OLGA <30398249+Olga-Pa@users.noreply.github.com>
2021-05-22 23:51:45 -0400

    input

--- src/components/ToDoForm.tsx
+++ src/components/ToDoForm.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export const Todo: React.FC = () => {
+  const [title, setTitle] = useState<string>('')
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value)
+  }
   return (
     <div className="Input-file">
-    <input type="text" id="title"></input>
-    <label htmlFor="title" className="active">what do  you want to do</label>
+      <label htmlFor="title" className="active mf2">What do  you want to do?</label>
+      <input
+        onChange={changeHandler}
+        value={title}
+        type="text"
+        id="title"
+        placeholder="what do you want to do">
+        </input>
+
     </div>
   )
 }
