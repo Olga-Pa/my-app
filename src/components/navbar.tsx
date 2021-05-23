1d98b4dc9662649409c1105365de57d34e0a25b8
OLGA <30398249+Olga-Pa@users.noreply.github.com>
2021-05-22 22:20:10 -0400

    styles

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Navbar: React.FC = () => (
   <nav>
-    <div className="nav-wrapper blue lighten-4">
-      <a href="#" className="brand-logo">React + TypeScript</a>
+    <div className="nav-wrapper blue lighten-4 pxq">
+      <a href="#" className="brand-logo pxq">React + TypeScript</a>
       <ul className="right hide-on-med-and-down">
         <li><a href="/">To do list</a></li>
         <li><a href="/">Informatin</a></li>
